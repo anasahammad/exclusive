5c7f9bd435d94916e324e6276525be31d77d96ea
Anas Ahammad <anasahammad2002@gmail.com>
2024-07-29 23:08:28 +0600

    mobile navbar added

---
+++ client-side/src/components/shared/MobileNav.tsx
@@ -0,0 +1,53 @@
+import { Link, useLocation } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { CiMenuFries } from "react-icons/ci";
+import { Input } from "../ui/input";
+import { IoSearchOutline } from "react-icons/io5";
+
+interface LinkProps {
+    path: string;
+    title: string;
+  }
+const MobileNav = () => {
+    const location = useLocation()
+    const navLinks:LinkProps[]  = [
+        {
+            path: '/',
+            title: 'Home'
+        },
+        {
+            path: '/contact',
+            title: 'Contact'
+        },
+        {
+            path: '/about',
+            title: 'About'
+        },
+        {
+            path: '/signup',
+            title: 'Sign Up'
+        },
+    ]
+    return (
+        <Sheet >
+            <SheetTrigger>
+                <CiMenuFries/>
+            </SheetTrigger>
+
+            <SheetContent className="flex flex-col ">
+            <div className="flex gap-2 items-center mt-6">
+                   <Input placeholder="What are you looking for?" className="  font-poppins  bg-[#F5F5F5]"/>
+                   <span className=" cursor-pointer  text-2xl"><IoSearchOutline /></span>
+                   </div>
+
+                   <nav className="flex flex-col justify-center items-center gap-6">
+                    {navLinks.map((link, index)=> (
+                        <Link className={`${link.path === location.pathname && 'border-b-2 border-[#000]'}text-[#000] text-[16px] font-poppins` } to={link.path} key={index}>{link.title}</Link>)
+                    )}
+                </nav>
+            </SheetContent>
+        </Sheet>
+    );
+};
+
+export default MobileNav;
--- client-side/src/components/shared/Navbar.tsx
+++ client-side/src/components/shared/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 import { Input } from "../ui/input";
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 import { FaRegHeart } from "react-icons/fa";
+import MobileNav from "./MobileNav";
 
 
 
@@ -33,7 +34,11 @@ const Navbar = () => {
     ]
     return (
         <header className="py-4 ">
-            <div className="container mx-auto flex justify-between items-center py-6 border-b-2 ">
+            <div className="container mx-auto ">
+                <div className="md:hidden">
+                <MobileNav />
+                </div>
+            <div className=" flex justify-between items-center py-6 border-b-2 ">
                 {/* logo */}
                 <Link to="/">
                     <h1 className="text-[#000] font-inter text-2xl font-bold">MobileMart</h1>
@@ -42,14 +47,16 @@ const Navbar = () => {
                 {/* desktop Navigation Menu */}
 
                 
-                <nav className="flex gap-6">
+               <div className="hidden md:block">
+               <nav className="flex gap-6  ">
                     {navLinks.map((link, index)=> (
-                        <Link className={`${link.path === location.pathname && 'border-b-2 border-[#000]'}text-[#000] text-[18px] font-poppins` } to={link.path} key={index}>{link.title}</Link>)
+                        <Link className={`${link.path === location.pathname && 'border-b-2 border-[#000]'}text-[#000] text-[16px] font-poppins` } to={link.path} key={index}>{link.title}</Link>)
                     )}
                 </nav>
+               </div>
 
                 <div className="flex gap-4 items-center">
-                   <div className="relative">
+                   <div className="relative hidden md:block">
                    <Input placeholder="What are you looking for?" className=" w-[250px] font-poppins  bg-[#F5F5F5]"/>
                    <span className="absolute cursor-pointer top-2 right-4 text-2xl"><IoSearchOutline /></span>
                    </div>
@@ -60,6 +67,7 @@ const Navbar = () => {
                    </div>
                 </div>
                 </div>
+            </div>
             
         </header>
     );
