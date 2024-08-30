0d834a2ee346d9816f9dc3a2bf6b2dc36f712177
Anas Ahammad <anasahammad2002@gmail.com>
2024-08-30 23:13:18 +0600

    initialize the server

---
+++ server-side/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const cors = require("cors")
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get("/", async(req, res)=>{
+    res.send("Exclusive server is running")
+})
+
+app.listen(port, ()=>{
+    console.log(`The server is running on port ${port}`)
+})
